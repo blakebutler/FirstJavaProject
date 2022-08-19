d483145de3e28071c482b54622e0452ac14a8651
BRB <blaketip05@gmail.com>
2022-08-19 10:30:04 -0500

    Initial commit

---
+++ src/FirstClass.java
@@ -0,0 +1,20 @@
+public class FirstClass
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+        System.out.println("Goodbye, World!\nwow");
+
+        int numDogs; // intstantiation
+        numDogs = 2; // initialization
+        int numCats = 0;// intstantiation & initialization
+
+        System.out.println("Dogs: " +numDogs +"\nCats: " +numCats);
+        System.out.println("At first there were " +numCats +" cats");
+
+        numCats += 1;
+        numCats ++;
+
+        System.out.println("Now there are " +numCats +" cats");
+    }
+}
